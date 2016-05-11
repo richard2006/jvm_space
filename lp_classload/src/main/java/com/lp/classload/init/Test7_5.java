30c61c1985b263e64d71b060593c79d97c7ee414
rlp2006 <rlp2006@126.com>
2016-05-11 10:20:24 +0800

    类初始化

---
+++ lp_classload/src/main/java/com/lp/classload/init/Test7_5.java
@@ -0,0 +1,14 @@
+package com.lp.classload.init;
+/**
+ * 静态语句块中只能访问到定义在静态语句块之前的变量，定义在它之后的变量，在前面的静态语句块可以赋值，但是不能访问
+ * @author Administrator
+ *
+ */
+public class Test7_5 {
+	static{
+		i=0;
+//		System.out.println(i);//非法向前变量引用
+	}
+	static int i;
+	
+}
